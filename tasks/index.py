8ff7aadca4fcbf840e3429c9391465a175f47547
Andrew Jackson <Andrew.Jackson@bl.uk>
2018-02-16 20:58:16 +0000

    Try running directly.

--- tasks/index.py
+++ tasks/index.py
@@ -104,11 +104,11 @@ if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
     luigi.interface.setup_interface_logging()
 
-    luigi.run(['CdxIndexAndVerify', '--local-scheduler', '--target-date', '2018-02-10'])
+    #luigi.run(['CdxIndexAndVerify', '--local-scheduler', '--target-date', '2018-02-10'])
 
-    #very = CdxIndexAndVerify(target_date=datetime.datetime.strptime("2018-02-10","%Y-%m-%d"))
-    #cdx = CheckCdxIndex(input_file=very.input().path)
-    #cdx.run()
+    very = CdxIndexAndVerify(target_date=datetime.datetime.strptime("2018-02-10","%Y-%m-%d"))
+    cdx = CheckCdxIndex(input_file=very.input().path)
+    cdx.run()
 
     #input = os.path.join(os.getcwd(),'test/input-list.txt')
     #luigi.run(['CheckCdxIndex', '--input-file', input, '--from-local', '--local-scheduler'])
